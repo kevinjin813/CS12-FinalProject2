2d42b4cd9eb65d9ffc33b853ed2f41ab7dd6cfca
kevinjin813 <55816605+kevinjin813@users.noreply.github.com>
2020-05-09 14:22:22 -0400

    Year: Constructor and recieve letter

--- main/Year.java
+++ main/Year.java
@@ -1,5 +1,21 @@
 package main;
 
 public class Year {
-
+	Month[] months=new Month[12];
+	Year next;
+	Year prev;
+	int year;
+	public Year(int year)
+	{
+		this.year=year;
+	}
+	
+	public void receiveLetter(int month,String mail)
+	{
+		if(month>12 || month<0)
+		{
+			throw new Exception();
+		}
+		months[month].takeIn(mail);
+	}
 }
