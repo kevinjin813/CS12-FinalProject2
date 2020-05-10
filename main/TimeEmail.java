ee5996c090fe7784ae46ff575f35351004a84516
yiming-zhang <62267333+yiming-zhang@users.noreply.github.com>
2020-05-10 23:13:39 +0800

    Update TimeEmail.java

--- main/TimeEmail.java
+++ main/TimeEmail.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 package main;
 
 public class TimeEmail {
 	public static void main(String[] args)
 	{
-		
+   System.out.println("Welcome to the timeemail system! You can read the message sent by yourself in the past, and also write a message to yourself that you can read in the future.");
+   Scanner in = new Scanner(System.in);
+   System.out.println("Do you want to read or write? Type in 1 for read and 2 for write.");
+   String read_or_write = in.nextLine();
+
 	}
 }
