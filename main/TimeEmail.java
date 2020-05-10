2c4f7fe8980d105a96448ad93e8999e07bdb8595
yiming-zhang <62267333+yiming-zhang@users.noreply.github.com>
2020-05-10 23:17:00 +0800

    Update TimeEmail.java
    adding code

--- main/TimeEmail.java
+++ main/TimeEmail.java
@@ -4,10 +4,19 @@ package main;
 public class TimeEmail {
 	public static void main(String[] args)
 	{
-   System.out.println("Welcome to the timeemail system! You can read the message sent by yourself in the past, and also write a message to yourself that you can read in the future.");
+   System.out.println("Welcome to the timeemail system! You can read the message sent by yourself in the past,and also write a message to yourself that you can read in the future.");
    Scanner in = new Scanner(System.in);
-   System.out.println("Do you want to read or write? Type in 1 for read and 2 for write.");
+   System.out.println("Do you want to read or write? Type in 1 for read and 2 for write.");//choose whether you want to read or write a email
    String read_or_write = in.nextLine();
+
+   if (read_or_write.equals("1")) { //if you choose to read
+    Scanner input = new Scanner(System.in);
+    System.out.println("Which month you want to read? ");
+    String month = input.nextLine();
+    Scanner inputt = new Scanner(System.in);
+    System.out.println("Do you want to read all of it or one specific message? Type in 1 for alll the message and 2 for a specific one. ");
+    String specific = inputt.nextLine();
+
 
 	}
 }
