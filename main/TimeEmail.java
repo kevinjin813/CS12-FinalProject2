84dba6629d6ea891d3e7ac0e7a15c05e484b9dc7
yiming-zhang <62267333+yiming-zhang@users.noreply.github.com>
2020-05-10 23:25:07 +0800

    Update TimeEmail.java

--- main/TimeEmail.java
+++ main/TimeEmail.java
@@ -16,14 +16,24 @@ public class TimeEmail {
     Scanner inputt = new Scanner(System.in);
     System.out.println("Do you want to read all of it or one specific message? Type in 1 for alll the message and 2 for a specific one. ");
     String specific = inputt.nextLine();
-
+      if(specific.equals("1")){
+      
+      }
+	   
       if (specific.equals("2")) {
        Scanner iinput = new Scanner(System.in);
        System.out.println("Which message do you want to read? Type in numbers ");
        Integer monthasd = iinput.nextInt();
    }
   }
-
-
-	}
+    else if (read_or_write.equals("2")) {
+     Scanner inn = new Scanner(System.in);
+     System.out.println("Which year and month do you want to read in the future?");
+     Integer year = inn.nextInt();
+     Integer month = inn.nextInt();
+     Scanner innn = new Scanner(System.in);
+     System.out.println("What's your message?");
+     String message = innn.nextLine();
+     }
+   }
 }
